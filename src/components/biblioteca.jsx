a1fec045272661682cc9ca084d66e5a9fa4cb496
Carlos <carlos.coelho@infraclearsalecom.onmicrosoft.com>
2022-06-28 15:09:32 -0300

    primeiro commit

---
+++ src/components/biblioteca.jsx
@@ -0,0 +1,13 @@
+import { defineComponent } from "vue";
+
+export const Biblioteca = defineComponent({
+  data() {
+    return {
+      livros: [],
+    };
+  },
+  methods: {},
+  render() {
+    return <div></div>;
+  },
+});
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <Biblioteca />
+</template>
+
+<script>
+// @ is an alias to /src
+import Biblioteca from "@/components/biblioteca.jsx";
+
+export default {
+  name: "Home",
+  components: {
+    Biblioteca,
+  },
+};
+</script>
